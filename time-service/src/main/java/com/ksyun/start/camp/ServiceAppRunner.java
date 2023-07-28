514d3df414c7de311fd42235fa30d96a26b5603d
libo14 <libo14@kingsoft.com>
2023-07-28 17:06:57 +0800

    feat:实现服务注册和定时发送心跳

--- time-service/src/main/java/com/ksyun/start/camp/ServiceAppRunner.java
+++ time-service/src/main/java/com/ksyun/start/camp/ServiceAppRunner.java
@@ -1,24 +1,37 @@
 package com.ksyun.start.camp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 /**
  * 服务启动运行逻辑
  */
 @Component
+@EnableScheduling
 public class ServiceAppRunner implements ApplicationRunner {
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        // 此处代码会在 Boot 应用启动时执行
-
-        // 开始编写你的逻辑，下面是提示
         // 1. 向 registry 服务注册当前服务
-        // 2. 定期发送心跳逻辑
-
         // TODO
+        restTemplate.getForObject("http://localhost:8200/api/register", ApiResponse.class);
+        System.out.println("服务启动运行逻辑:--->" + "向 registry 服务注册当前服务");
+    }
+
+    @Scheduled(cron = "*/3 * * * * ?")
+    private void printNowDate() {
+        // 2. 定期发送心跳逻辑
+        // TODO
+        restTemplate.getForObject("http://localhost:8200/api/heartbeat", ApiResponse.class);
+        long nowDateTime = System.currentTimeMillis();
+        System.out.println("固定定时任务执行:--->" + nowDateTime + "，此任务为每3秒执行一次");
     }
 }
