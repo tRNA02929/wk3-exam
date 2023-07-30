8978e45b0b373c8fe7d0109f284b5e7954811ba8
libo14 <libo14@kingsoft.com>
2023-07-30 15:08:29 +0800

    add:添加了rest模块用于处理返回的json格式，此模块源自于trade-order模板中的biz/biz-glue模块

---
+++ rest/src/main/java/com/ksyun/start/camp/rest/RestResult.java
@@ -0,0 +1,77 @@
+package com.ksyun.start.camp.rest;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 响应DTO
+ *
+ * @author libo14
+ **/
+@Getter
+@NoArgsConstructor
+public class RestResult<T> implements Serializable {
+
+    // serialVersionUID
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 异常代码
+     */
+    private int code;
+
+    /**
+     * 异常信息
+     */
+    private String msg;
+
+    private String descr;
+
+
+    /**
+     * 返回结果
+     */
+    private T data;
+
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * 链式方法
+     */
+    public RestResult<T> code(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public RestResult<T> msg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public RestResult<T> descr(String descr) {
+        this.descr = descr;
+        return this;
+    }
+
+    public RestResult<T> data(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public static RestResult success() {
+        RestResult dto = new RestResult();
+        dto.code(RestConsts.DEFAULT_SUCCESS_CODE).msg(RestConsts.SUCCESS_MESSAGE);
+        return dto;
+    }
+
+    public static RestResult failure() {
+        RestResult dto = new RestResult();
+        dto.code(500).msg(RestConsts.ERROR_MESSAGE);
+        return dto;
+    }
+}
