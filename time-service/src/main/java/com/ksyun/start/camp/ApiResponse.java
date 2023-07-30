9e2ee33bd0b0b6b134c78b89547ea1ad0f5055d4
libo14 <libo14@kingsoft.com>
2023-07-30 15:10:10 +0800

    refactor:简化模板中的格式

--- time-service/src/main/java/com/ksyun/start/camp/ApiResponse.java
+++ time-service/src/main/java/com/ksyun/start/camp/ApiResponse.java
@@ -1,25 +1,21 @@
 package com.ksyun.start.camp;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 /**
  * 代表此 API 的返回对象
  */
 @Data
-@NoArgsConstructor
+@Builder
 public class ApiResponse {
 
-    /**
-     * 代表此 API 的响应返回码
-     * 200 表示成功，非 200 表示失败
-     */
-    private int code;
+    private Long serviceId;
 
-    private String data;
+    private String result;
 
-    public ApiResponse(int code, String data) {
-        this.code = code;
-        this.data = data;
-    }
 }
