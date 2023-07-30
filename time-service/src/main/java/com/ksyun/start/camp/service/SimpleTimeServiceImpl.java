eec17e6c977a9673d74c664df74f62650fc2f6c0
libo14 <libo14@kingsoft.com>
2023-07-30 15:09:37 +0800

    feat:实现getDateTime方法

--- time-service/src/main/java/com/ksyun/start/camp/service/SimpleTimeServiceImpl.java
+++ time-service/src/main/java/com/ksyun/start/camp/service/SimpleTimeServiceImpl.java
@@ -2,6 +2,10 @@ package com.ksyun.start.camp.service;
 
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * 代表简单时间服务实现
  */
@@ -13,7 +17,14 @@ public class SimpleTimeServiceImpl implements SimpleTimeService {
 
         // 开始编写简单时间服务的核心逻辑
         // 获取时间、格式化时间、返回
-
-        return null;
+        if (style == null || style.equals("") || style.equals("full")) {
+            return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        } else if (style.equals("date")) {
+            return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } else if (style.equals("time")) {
+            return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        } else {
+            return String.valueOf(Instant.now().toEpochMilli());
+        }
     }
 }
