db72288d4c92851eb12b298ae22d23f2812f3ad1
libo14 <libo14@kingsoft.com>
2023-07-30 19:41:11 +0800

    feat:所有接口都使用restresult包裹，标准化格式

--- registry/src/main/java/com/ksyun/start/camp/service/RegistryService.java
+++ registry/src/main/java/com/ksyun/start/camp/service/RegistryService.java
@@ -2,10 +2,15 @@ package com.ksyun.start.camp.service;
 
 import com.ksyun.start.camp.dto.RegisteredServiceDTO;
 import com.ksyun.start.camp.entity.ServiceEntity;
+import com.ksyun.start.camp.rest.RestResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Slf4j(topic = "RegistryService")
 public class RegistryService {
 
     @Autowired
@@ -15,21 +20,42 @@ public class RegistryService {
         registeredServices.clearExpiredService();
         serviceEntity.setHeartBeatTime(System.currentTimeMillis());
         boolean b = registeredServices.addService(serviceEntity);
-        return registeredServices.getServices().toArray();
+        if (b) {
+            log.info("注册成功,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+            return RestResult.success().descr("注册成功").data(serviceEntity);
+        }
+        log.info("注册失败,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+        return RestResult.failure().descr("服务已注册").data(serviceEntity);
     }
 
     public Object unregister(ServiceEntity serviceEntity) {
         boolean b = registeredServices.removeService(serviceEntity);
-        return registeredServices.getServices().toArray();
+//        return registeredServices.getServices().toArray();
+        if (b) {
+            log.info("服务注销成功,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+            return RestResult.success().descr("服务已注销").data(serviceEntity);
+        }
+        log.info("服务注销失败,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+        return RestResult.failure().descr("未找到该服务").data(serviceEntity);
     }
 
     public Object heartbeat(ServiceEntity serviceEntity) {
         registeredServices.clearExpiredService();
-        return registeredServices.heartbeat(serviceEntity);
+        boolean b = registeredServices.heartbeat(serviceEntity);
+        if (b) {
+            log.info("心跳更新成功,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+            return RestResult.success().descr("心跳更新成功").data(serviceEntity);
+        }
+        log.info("心跳更新失败,服务ID:{},ip:{},端口:{}", serviceEntity.getServiceId(), serviceEntity.getIpAddress(), serviceEntity.getPort());
+        return RestResult.failure().descr("心跳更新失败").data(serviceEntity);
     }
 
     public Object discovery(String serviceName) {
         registeredServices.clearExpiredService();
-        return registeredServices.discovery(serviceName);
+        List<ServiceEntity> serviceEntities = registeredServices.discovery(serviceName);
+        if (serviceEntities != null && serviceEntities.size() > 0) {
+            return RestResult.success().descr("查询成功").data(serviceEntities);
+        }
+        return RestResult.failure().descr("未找到该服务").data(serviceEntities);
     }
 }
