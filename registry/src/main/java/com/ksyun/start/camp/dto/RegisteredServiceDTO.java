abacd736cf825e1f370ff6eeeda46c8dc903f497
libo14 <libo14@kingsoft.com>
2023-08-01 13:45:12 +0800

    feat: 🎉 使用TreeSet作为服务的存储的数据结构，可以根据轮询次数进行排序

--- registry/src/main/java/com/ksyun/start/camp/dto/RegisteredServiceDTO.java
+++ registry/src/main/java/com/ksyun/start/camp/dto/RegisteredServiceDTO.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,10 +21,14 @@ public class RegisteredServiceDTO {
     @Value("${heart}")
     private int heart;
 
-    private HashSet<ServiceEntity> services;
+    // 使用TreeSet，按照轮询次数自动排序
+    private SortedSet<ServiceEntity> clients;
+
+    private SortedSet<ServiceEntity> timeServices;
 
     public RegisteredServiceDTO() {
-        this.services = new HashSet<>();
+        this.clients = new TreeSet<>((o1, o2) -> o1.getPollCount() - o2.getPollCount());
+        this.timeServices = new TreeSet<>((o1, o2) -> o1.getPollCount() - o2.getPollCount());
     }
 
     /**
@@ -30,11 +36,14 @@ public class RegisteredServiceDTO {
      * 服务的id相同，就认为是同一个服务；
      * 服务的地址一样，就认为是同一个服务
      *
-     * @param service
+     * @param service 服务
      * @return boolean 是否添加成功
      */
     public boolean addService(ServiceEntity service) {
-        return services.add(service);
+        if (service.getServiceName().equals("time-service")) {
+            return timeServices.add(service);
+        }
+        return clients.add(service);
     }
 
     /**
@@ -45,8 +54,9 @@ public class RegisteredServiceDTO {
      * @return boolean 是否删除成功
      */
     public boolean removeService(ServiceEntity service) {
+        SortedSet<ServiceEntity> goalService = getGoalService(service.getServiceName());
         ServiceEntity temp = null;
-        for (ServiceEntity serviceEntity : services) {
+        for (ServiceEntity serviceEntity : goalService) {
             if (serviceEntity.getServiceId().equals(service.getServiceId())
                     && serviceEntity.getServiceName().equals(service.getServiceName())
                     && serviceEntity.equalsAddress(service.getIpAddress(), service.getPort())) {
@@ -54,7 +64,7 @@ public class RegisteredServiceDTO {
                 break;
             }
         }
-        return services.remove(temp);
+        return goalService.remove(temp);
     }
 
     /**
@@ -65,7 +75,8 @@ public class RegisteredServiceDTO {
      * @return boolean 是否更新成功
      */
     public boolean heartbeat(ServiceEntity service) {
-        for (ServiceEntity serviceEntity : services) {
+        SortedSet<ServiceEntity> goalService = getGoalService(service.getServiceName());
+        for (ServiceEntity serviceEntity : goalService) {
             if (serviceEntity.getServiceId().equals(service.getServiceId())
                     && serviceEntity.equalsAddress(service.getIpAddress(), service.getPort())) {
                 serviceEntity.setHeartBeatTime(System.currentTimeMillis());
@@ -84,9 +95,12 @@ public class RegisteredServiceDTO {
      */
     public List<ServiceEntity> discovery(String serviceName) {
         if (serviceName == null) {
-            return new ArrayList<>(services);
+            List<ServiceEntity> result = new ArrayList<>(timeServices);
+            result.addAll(clients);
+            return result;
         }
-        List<ServiceEntity> result = services.stream()
+        SortedSet<ServiceEntity> goalService = getGoalService(serviceName);
+        List<ServiceEntity> result = goalService.stream()
                 .filter(serviceEntity -> serviceEntity.getServiceName().equals(serviceName))
                 .collect(Collectors.toList());
         return result;
