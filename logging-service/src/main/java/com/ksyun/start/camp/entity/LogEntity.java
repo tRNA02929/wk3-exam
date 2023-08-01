e285de0b73cc9f33975d5dd0d1a1c651e5015299
libo14 <libo14@kingsoft.com>
2023-08-01 22:30:57 +0800

    feat: ✨ 重写hashCode，equals，compareTo用于去重和排序

--- logging-service/src/main/java/com/ksyun/start/camp/entity/LogEntity.java
+++ logging-service/src/main/java/com/ksyun/start/camp/entity/LogEntity.java
@@ -4,8 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
-public class LogEntity {
+public class LogEntity implements Comparable<LogEntity> {
 
     private Integer logId;
 
@@ -19,4 +18,33 @@ public class LogEntity {
 
     private String message;
 
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || !(obj instanceof LogEntity)) {
+            return false;
+        }
+        LogEntity logEntity = (LogEntity) obj;
+        if (logEntity.getServiceName() == null || !serviceName.equals(logEntity.getServiceName()) ||
+                logEntity.getServiceId() == null || !serviceId.equals(logEntity.getServiceId()) ||
+                logEntity.getDatetime() == null || !datetime.equals(logEntity.getDatetime()) ||
+                logEntity.getLevel() == null || !level.equals(logEntity.getLevel()) ||
+                logEntity.getMessage() == null || !message.equals(logEntity.getMessage())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 重写 compareTo 方法，用于排序
+     */
+    @Override
+    public int compareTo(LogEntity o) {
+        return o.getLogId() - logId;
+    }
 }
